961dd0c75556cae0fdded00fb23dc19f8bddb878
bodge-it <badimba@gmail.com>
2025-11-11 19:24:36 +0000

    Reset to dev

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'rc1';
+    public const EXTRA_VERSION = 'rc2-dev';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Release Candidate';
+    public const DEV_STATUS = 'Development';
 
     /**
      * Code name.
@@ -99,7 +99,7 @@ final class Version
      * @since  3.5
      */
 
-    public const RELTIME = '19:30';
+    public const RELTIME = '19:31';
 
     /**
      * Release timezone.
