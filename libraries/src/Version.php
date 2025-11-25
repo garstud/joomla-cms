2ce53155d74ced5ef531768fccf925e141da5d7c
Harald Leithner <leithner@itronic.at>
2025-11-25 21:56:43 +0100

    Reset to dev

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'alpha1';
+    public const EXTRA_VERSION = 'alpha2-dev';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Alpha';
+    public const DEV_STATUS = 'Development';
 
     /**
      * Code name.
@@ -99,7 +99,7 @@ final class Version
      * @since  3.5
      */
 
-    public const RELTIME = '21:15';
+    public const RELTIME = '20:56';
 
     /**
      * Release timezone.
