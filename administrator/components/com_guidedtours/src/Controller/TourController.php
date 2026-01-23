30d8ddc647de464262d729d0d05be8239e8bc076
Marc STUDER <dev@garstud.com>
2026-01-23 15:54:04 +0100

    Update TourController.php

--- administrator/components/com_guidedtours/src/Controller/TourController.php
+++ administrator/components/com_guidedtours/src/Controller/TourController.php
@@ -23,7 +23,19 @@ use Joomla\CMS\MVC\Controller\FormController;
  */
 class TourController extends FormController
 {
-            // Check for request forgeries.
+    /**
+     * Method to save a record.
+     *
+     * @param   string  $key     The name of the primary key of the URL variable.
+     * @param   string  $urlVar  The name of the URL variable if different from the primary key (sometimes required to avoid router collisions).
+     *
+     * @return  boolean  True if successful, false otherwise.
+     *
+     * @since   1.6
+     */
+	public function save($key = null, $urlVar = null)
+    {    
+        // Check for request forgeries.
         $this->checkToken();
 
         $model   = $this->getModel();
